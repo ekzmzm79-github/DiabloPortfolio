b3f5c7916f04598c1e1c57fe89d8568fbe1005d7
ekzmzm79-github <57693531+ekzmzm79-github@users.noreply.github.com>
2020-08-06 18:52:31 +0900

    MakePlayerCharacter-4
    캐릭터 모델과 애니메이션을 StateMachine을 이용하여 각 상황에 맞게 재생시키기
    (이동중에는 walking, 대기 시에는 idle 모션 3개가 랜덤 재생)

---
+++ Diablo/Assets/IdleRandomStateMachineBehaviour.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IdleRandomStateMachineBehaviour : StateMachineBehaviour
+{
+
+    #region Variables
+    public int numberOfStates = 3; //애니메이션 개수
+    public float minNormTime = 0f; 
+    public float maxNormTime = 5f;
+
+    public float randomNormalTime;
+
+    readonly int hashRandomIdle = Animator.StringToHash("RandomIdle");
+    #endregion Variables
+
+    // OnStateEnter is called when a transition starts and the state machine starts to evaluate this state
+    override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        //트랜지션에 필요한 시간을 랜덤하게 결정
+        randomNormalTime = Random.Range(minNormTime, maxNormTime);
+    }
+
+    // OnStateUpdate is called on each Update frame between OnStateEnter and OnStateExit callbacks
+    override public void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        //만약 베이스 레이어에 있는 상황이라면 -1로 초기화해서 아무런 랜덤idle 실행안함
+        if(animator.IsInTransition(0) && animator.GetCurrentAnimatorStateInfo(0).fullPathHash == stateInfo.fullPathHash)
+        {
+            animator.SetInteger(hashRandomIdle, -1);
+            
+        }
+
+        //베이스 레이어에 있는 상황이 아니고 현재 재생중인 애니메이션이 randomNormalTime보다 크다면?
+        //-> 해당 idle 애니메이션 재생 시간을 초과했으므로 교체해야함
+        if (!animator.IsInTransition(0) && stateInfo.normalizedTime > randomNormalTime)
+        {
+            animator.SetInteger(hashRandomIdle, Random.Range(0, numberOfStates));
+        }
+
+    }
+
+    // OnStateExit is called when a transition ends and the state machine finishes evaluating this state
+    //override public void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    //{
+    //    
+    //}
+
+    // OnStateMove is called right after Animator.OnAnimatorMove()
+    //override public void OnStateMove(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    //{
+    //    // Implement code that processes and affects root motion
+    //}
+
+    // OnStateIK is called right after Animator.OnAnimatorIK()
+    //override public void OnStateIK(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    //{
+    //    // Implement code that sets up animation IK (inverse kinematics)
+    //}
+}
---
+++ Diablo/Assets/Scripts/PlayerCharacter.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+using UnityEngine.EventSystems;
+
+[RequireComponent(typeof(NavMeshAgent)), RequireComponent(typeof(CharacterController)), RequireComponent(typeof(Animator))]
+
+public class PlayerCharacter : MonoBehaviour
+{
+    #region Variables
+
+    private CharacterController characterController;
+    [SerializeField]
+    private LayerMask groundLayerMask;
+
+    private NavMeshAgent agent;
+    private Camera camera;
+
+    [SerializeField]
+    private Animator animator;
+
+    //애니메이터에 존재하는 변수를 해쉬변환을 통해 int 변수로 받아서 세팅
+    readonly int moveHash = Animator.StringToHash("Move"); 
+    readonly int fallingHash = Animator.StringToHash("Falling");
+    #endregion
+
+    #region Main Methods
+
+
+    
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        characterController = GetComponent<CharacterController>();
+        agent = GetComponent<NavMeshAgent>();
+        agent.updatePosition = false; // 네비메쉬 에이전트 내부의 이동기능 끼능
+        agent.updateRotation = true;
+
+        camera = Camera.main;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //마우스 왼쪽 클릭한 지점으로 캐릭터가 이동한다.(NavMeshAgent를 사용해서)
+
+        //마우스 왼쪽 클릭 입력 받기
+        if (Input.GetMouseButtonDown(0))
+        {
+            //씬에서 월드로 향하는 레이캐스트
+            Ray ray = camera.ScreenPointToRay(Input.mousePosition);
+
+            //hit 체크
+            RaycastHit hit;
+            if (Physics.Raycast(ray, out hit, 100, groundLayerMask))
+            {
+                Debug.Log("We hited! " + hit.collider.name + " " + hit.point);
+
+                //캐릭터를 hit된 곳으로 이동
+                agent.SetDestination(hit.point);
+            }
+        }
+
+        //만약 agent의 스톱거리보다 목적지 거리가 더 크다면 -> 아직 더 가야한다
+        if (agent.remainingDistance > agent.stoppingDistance)
+        {
+
+            characterController.Move(agent.velocity * Time.deltaTime);
+            animator.SetBool(moveHash, true);
+        }
+        else
+        {
+            //그게 아니라면 스톱
+            characterController.Move(Vector3.zero);
+            animator.SetBool(moveHash, false);
+        }
+
+
+        /*
+        if(agent.isOnOffMeshLink)
+        {
+            animator.SetBool(fallingHash, agent.velocity.y != 0.0f);
+        }
+
+
+
+        */
+
+    }
+
+    private void OnAnimatorMove()
+    {
+        Vector3 position = agent.nextPosition;
+        animator.rootPosition = agent.nextPosition;
+        transform.position = position;
+    }
+    #endregion
+}
