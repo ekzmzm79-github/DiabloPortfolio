2d4dfbdee08ccdbac17cf0e448b80922f6c51c9d
ekzmzm79-github <57693531+ekzmzm79-github@users.noreply.github.com>
2020-08-06 17:03:46 +0900

    MakePlayerCharacter-3
     NavMeshAgent를 이용하여ControllerCharacter 스크립트를 수정
    (마우스 왼쪽 클릭한 지점으로 장애물을 피해서 자동으로 이동)

--- Diablo/Assets/Scripts/ControllerCharacter.cs
+++ Diablo/Assets/Scripts/ControllerCharacter.cs
@@ -1,19 +1,14 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.AI;
 public class ControllerCharacter : MonoBehaviour
 {
     #region Variables
-    public float speed = 5.0f;
-    public float jumpHeight = 2.0f;
-    public float dashDistance = 5.0f;
-
-    public float gravity = -9.81f;
-    public Vector3 drags;
 
     private CharacterController characterController;
-    private Vector3 inputDirection = Vector3.zero;
+    private NavMeshAgent agent;
+    private Camera camera;
 
     private bool isGrounded = false;
     public LayerMask groundLayerMask;
@@ -27,79 +22,53 @@ public class ControllerCharacter : MonoBehaviour
     void Start()
     {
         characterController = GetComponent<CharacterController>();
+        agent = GetComponent<NavMeshAgent>();
+        agent.updatePosition = false; // 네비메쉬 에이전트 내부의 이동기능 끼능
+        agent.updateRotation = true;
+
+        camera = Camera.main;
     }
 
     // Update is called once per frame
     void Update()
     {
-        //현재 캐릭터가 땅인지 아닌지를 검사
+        //마우스 왼쪽 클릭한 지점으로 캐릭터가 이동한다.(NavMeshAgent를 사용해서)
 
-        isGrounded = characterController.isGrounded;
-        if (isGrounded && calcVelocity.y < 0)
+        //마우스 왼쪽 클릭 입력 받기
+        if (Input.GetMouseButtonDown(0))
         {
-            calcVelocity.y = 0;
+            //씬에서 월드로 향하는 레이캐스트
+            Ray ray = camera.ScreenPointToRay(Input.mousePosition);
+
+            //hit 체크
+            RaycastHit hit;
+            if (Physics.Raycast(ray, out hit, 100, groundLayerMask))
+            {
+                Debug.Log("We hited! " + hit.collider.name + " " + hit.point);
+
+                //캐릭터를 hit된 곳으로 이동
+                agent.SetDestination(hit.point);
+            }
+
+
+            //만약 agent의 스톱거리보다 목적지 거리가 더 크다면 -> 아직 더 가야한다
+            if (agent.remainingDistance > agent.stoppingDistance)
+            {
+                characterController.Move(agent.velocity * Time.deltaTime);
+            }
+            else
+            {
+                //그게 아니라면 스톱
+                characterController.Move(Vector3.zero);
+            }
+
         }
-        //CheckGroundStatus();
-
-        //사용자의 입력을 이동입력 받기
-        Vector3 move = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
-        characterController.Move(move * Time.deltaTime * speed);
-        if (move != Vector3.zero)
-        {
-            transform.forward = inputDirection;
-        }
-
-        //점프 입력 처리
-        if (Input.GetButtonDown("Jump") && isGrounded)
-        {
-            calcVelocity.y += Mathf.Sqrt(jumpHeight * -2.0f * Physics.gravity.y);
-        }
-
-        //대쉬 입력 처리
-        if (Input.GetButtonDown("Dash"))
-        {
-            Vector3 dashVelocity = Vector3.Scale(transform.forward,
-                dashDistance * new Vector3(
-                (Mathf.Log(1f / (Time.deltaTime * drags.x + 1)) / -Time.deltaTime),
-                0,
-                (Mathf.Log(1f / (Time.deltaTime * drags.z + 1)) / -Time.deltaTime)
-                ));
-
-            calcVelocity += dashVelocity;
-        }
-
-        //중력값 계산
-        calcVelocity.y += gravity * Time.deltaTime;
-
-        calcVelocity.x /= 1 + drags.x * Time.deltaTime;
-        calcVelocity.y /= 1 + drags.y * Time.deltaTime;
-        calcVelocity.z /= 1 + drags.z * Time.deltaTime;
-
-        characterController.Move(calcVelocity * Time.deltaTime);
     }
 
-    #region Helper Methods
 
-    void CheckGroundStatus()
+    private void LateUpdate()
     {
-        RaycastHit hitInfo;
-
-#if UNITY_EDITOR
-        Debug.DrawLine(transform.position + (Vector3.up * 0.1f),
-            transform.position + (Vector3.up * 0.1f) + (Vector3.down * groundCheckDistance));
-#endif
-
-        //캐릭터의 바닥에서(조금 위) 아래 방향으로 레이캐스트 발사
-        if (Physics.Raycast(transform.position + (Vector3.up * 0.1f), Vector3.down, out hitInfo, groundCheckDistance, groundLayerMask))
-        {
-            isGrounded = true;
-        }
-        else
-        {
-            isGrounded = false;
-        }
+        transform.position = agent.nextPosition;
     }
-
-    #endregion Helper Methods
 
 }
