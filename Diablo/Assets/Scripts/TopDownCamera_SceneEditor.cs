fa788fc851cc71ff597818535a61ba8b42549001
ekzmzm79-github <57693531+ekzmzm79-github@users.noreply.github.com>
2020-08-07 17:12:21 +0900

    MakeCamera
    Make "TopDownCamera" Script and Edit this Camera using "TopDownCamera_SceneEditior" Script
    
    (In other words.
    "TopDownCamera" Script -> Following Character Camera(Smooth Move)
    "TopDownCamera_SceneEditior" -> This main Camarea editing script
    (Using Sliders and Labels) . This way will make better debug enviroment.

---
+++ Diablo/Assets/Scripts/TopDownCamera_SceneEditor.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(TopDownCamera))]
+//타켓 카메라에 대한 커스터마이징
+public class TopDownCamera_SceneEditor : Editor
+{
+    #region Variables
+    private TopDownCamera targetCamera;
+    #endregion Variables
+
+    public override void OnInspectorGUI()
+    {
+        
+        targetCamera = (TopDownCamera)target;
+        base.OnInspectorGUI();
+    }
+
+    private void OnSceneGUI()
+    {
+        if(!targetCamera || !targetCamera.target)
+        {
+            return;
+        }
+
+        Transform cameraTarget = targetCamera.target; // 캐릭터
+        Vector3 targetPosition = cameraTarget.position;
+        targetPosition.y += targetCamera.lookAtHeight; //캐릭터 높이 만큼 더해줌
+
+        // 거리크기의 반지름을 가진 원을 그림
+        Handles.color = new Color(1f, 0f, 0f, 0.15f);
+        Handles.DrawSolidDisc(targetPosition, Vector3.up, targetCamera.distance);
+
+        Handles.color = new Color(0f, 1f, 0f, 0.75f);
+        Handles.DrawWireDisc(targetPosition, Vector3.up, targetCamera.distance);
+
+        // 슬라이더 추가
+        Handles.color = new Color(1f, 0f, 0f, 0.5f);
+        targetCamera.distance = Handles.ScaleSlider(targetCamera.distance, 
+            targetPosition, -cameraTarget.forward, 
+            Quaternion.identity, targetCamera.distance, 0.1f);
+        targetCamera.distance = Mathf.Clamp(targetCamera.distance, 2f, float.MaxValue);//최대, 최소값 설정
+
+        Handles.color = new Color(0f, 0f, 1f, 0.5f);
+        targetCamera.height = Handles.ScaleSlider(targetCamera.height,
+            targetPosition, Vector3.up,
+            Quaternion.identity, targetCamera.height, 0.1f);
+        targetCamera.height = Mathf.Clamp(targetCamera.height, 2f, float.MaxValue);//최대, 최소값 설정
+
+        //레이블 달기
+        GUIStyle labelStyle = new GUIStyle();
+        labelStyle.fontSize = 15;
+        labelStyle.normal.textColor = Color.white;
+
+        labelStyle.alignment = TextAnchor.UpperCenter;
+        Handles.Label(targetPosition + (-cameraTarget.forward * targetCamera.distance), "Distance", labelStyle);
+
+        labelStyle.alignment = TextAnchor.MiddleRight;
+        Handles.Label(targetPosition + (Vector3.up * targetCamera.height), "Height", labelStyle);
+
+        targetCamera.HandleCamera(); // 슬라이더로 변환된 값이 바로 적용되도록 호출
+    }
+
+}
---
+++ Diablo/Assets/TopDownCamera.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TopDownCamera : MonoBehaviour
+{
+    #region Variables
+    public float height = 5f;
+    public float distance = 10f;
+    public float angle = 45f;
+    public float lookAtHeight = 2f;
+    public float smoothSpeed = 0.5f;
+
+    private Vector3 refVelocity;
+
+    public Transform target;
+
+    #endregion Variables
+
+    private void LateUpdate()
+    {
+        HandleCamera();
+    }
+
+    public void HandleCamera()
+    {
+        if (!target)
+            return;
+
+        // worldPosition 만들기 (타겟보다 약간 뒤쪽에 있어야 하므로 -distance
+        Vector3 worldPosition = (Vector3.forward * -distance) + (Vector3.up * height);
+        //Debug.DrawLine(target.position, worldPosition, Color.red);
+
+        //회전 벡터
+        Vector3 rotatedVector = Quaternion.AngleAxis(angle, Vector3.up) * worldPosition;
+        //Debug.DrawLine(target.position, rotatedVector, Color.green);
+
+        Vector3 finalTargetPosition = target.position;
+        finalTargetPosition.y += lookAtHeight; // 현재 타겟(캐릭터의 중심점은 발바닥이므로 높이값을 더해줌)
+
+        Vector3 finalPosition = finalTargetPosition + rotatedVector;
+        //Debug.DrawLine(target.position, finalPosition, Color.blue);
+
+        transform.position = Vector3.SmoothDamp(transform.position, finalPosition, ref refVelocity, smoothSpeed);
+        transform.LookAt(target.position);
+    }
+
+    /// <summary>
+    /// 디버그용 위치확인 라인 및 원구 그리기
+    /// </summary>
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = new Color(1f, 0f, 0f, 0.5f);
+        if(target)
+        {
+            Vector3 lookAtPosition = target.position;
+            lookAtPosition.y += lookAtHeight;
+            Gizmos.DrawLine(transform.position, lookAtPosition);
+            Gizmos.DrawSphere(lookAtPosition, 0.25f); //바라보는 방향에 원구형태 그리기
+        }
+
+        Gizmos.DrawSphere(transform.position, 0.25f);
+    }
+}
